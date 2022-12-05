19a562529842328af0e698292a3e4a7f7998a5b7
liacdev <jagoblacksky@gmail.com>
2022-12-04 19:20:53 -0600

    Circle button

--- App.js
+++ App.js
@@ -10,7 +10,9 @@ import * as ImagePicker from 'expo-image-picker';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
-  
+ 
+  const[showAppOptions, setShowAppOptions] = useState(false);
+
   const [selectedImage, setSelectedImage] = useState(null);
 
   const pickImageAsync = async () => {
@@ -21,6 +23,7 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
@@ -35,12 +38,16 @@ export default function App() {
           selectedImage={selectedImage}
         />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+      {showAppOptions ? (
+        <View /> 
+        ) : (
+          <View style={styles.footerContainer}>
+            <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+            <Button label="Use this photo" opPress={() => setShowAppOptions(true)} />
+          </View>
+        )}
+        <StatusBar style="auto" />
+      </View>  
   );
 }
 

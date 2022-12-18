93a074bf278fb4d5f1056806f511bd63dd92fce3
liacdev <jagoblacksky@gmail.com>
2022-12-18 16:35:21 -0600

    Capture and save screenshot

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+
+import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
+import { captureRef } from 'react-native-view-shot';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 
-import * as ImagePicker from 'expo-image-picker';
-import * as MediaLibrary from 'expo-media-library';
 
 import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
@@ -21,7 +23,30 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  
   const [status, requestPermission] = MediaLibrary.usePermissions();
+  
+  if (status === null) {
+    requestPermission();
+  }
+  
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440, 
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const imageRef = useRef();
 
   const [pickedEmoji, setPickedEmoji] = useState(null);
 
@@ -35,10 +60,6 @@ export default function App() {
       allowsEditing: true,
       quality: 1,
     });
-
-    if (status === null) {
-      requestPermission();
-    }
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
@@ -60,15 +81,13 @@ export default function App() {
     setIsModalVisible(false);
   };
 
-  const onSaveImageAsync = () => {
-    // add more later
-  };
-
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        </View>
       </View>
 
       {showAppOptions ? (
